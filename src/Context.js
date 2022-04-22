bd69f817d9c5c530c17aabb61fb817cce0dad974
ranjanakash <akashbitm787@gmail.com>
2022-04-22 20:47:17 +0530

    e-car-rental

---
+++ src/Context.js
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState } from "react";
+import faker from "faker";
+import ImageAsset from '../src/Sedan-Transparent.png';
+
+const Cart = createContext();
+faker.seed(100);
+
+const Context = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const productsArray = [...Array(20)].map((p) => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: ImageAsset,
+  }));
+
+  const [products] = useState(productsArray);
+
+  return (
+    <Cart.Provider value={{ cart, setCart, products }}>
+      {children}
+    </Cart.Provider>
+  );
+};
+
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
